cbb1b5bbb5cc8cdb83a1ff70375491b61999fa54
Dinesh <dinesh.adhikari@skillbox.co.in>
2019-08-18 12:32:36 +0530

    auth creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,53 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControlName } from "@angular/forms";
+import { Component } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControlName
+} from "@angular/forms";
+
+import { AuthenticationService } from "./services/authentication.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'User-Login'
-  submitted: boolean
-  loginForm: FormGroup
+  title = "User-Login";
+  submitted: boolean;
+  loginForm: FormGroup;
 
-constructor(
-  private formBuilder: FormBuilder
-){}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthenticationService
+  ) {}
 
-
-ngOnInit() {
-  this.loginForm = this.formBuilder.group({
-    email: ["", Validators.required],
-    password: ["", Validators.required]
-  })
- 
-}
-
-
-onSubmit() {
-  this.submitted = true
-  if (this.loginForm.invalid) {
-    return;
-  }
-  let body = {
-    "email" : this.loginForm.value.email, 
-    'password' : this.loginForm.value.password
-   }
-   console.log(body)
-
-
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required]
+    });
   }
 
+  onSubmit() {
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+    let body = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password
+    };
 
+    this.authService.login(body).subscribe((data: any) => {
+      //your will data success or false
+      if (data.success) {
+        //do some action
+      } else {
+        //do some action
+        //show error mesage
+      }
+    })
+  }
 }
-
-
-
-
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from "@angular/common/http";
+import { map } from "rxjs/operators";
+
+import {environment} from "../../environments/environment"
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  baseUrl = environment.apiUrl;
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+
+
+  login(body) {
+    return this.http
+      .post<any>(this.baseUrl + "user-login", body)
+      .pipe(
+        map(user => {
+          if (user.success) {
+            localStorage.setItem("IsLogin", "true")
+          } 
+          return user
+        })
+      )
+  }
+
+
+
 }
