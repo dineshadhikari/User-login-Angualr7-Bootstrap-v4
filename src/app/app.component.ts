1aa51ca14aa4b65d7f0e83876b4d1d89e576177f
Dinesh <dinesh.adhikari@skillbox.co.in>
2019-08-18 10:55:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControlName } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'User-Login';
+  title = 'User-Login'
+  submitted: boolean
+  loginForm: FormGroup
+
+constructor(
+  private formBuilder: FormBuilder
+){}
+
+
+ngOnInit() {
+  this.loginForm = this.formBuilder.group({
+    email: ["", Validators.required],
+    password: ["", Validators.required]
+  })
+ 
 }
+
+
+onSubmit() {
+  this.submitted = true
+  if (this.loginForm.invalid) {
+    return;
+  }
+  let body = {
+    "email" : this.loginForm.value.email, 
+    'password' : this.loginForm.value.password
+   }
+   console.log(body)
+
+
+  }
+
+
+}
+
+
+
+
